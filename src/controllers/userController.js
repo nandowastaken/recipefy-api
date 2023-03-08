7615abd82bc54579ff48ded6ab884bebb66d5a95
Eliel Cirino <elielscirino@gmail.com>
2023-03-07 22:09:11 -0300

    exports.func -> module.exports = {}

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,51 +1,54 @@
 const Users = require('../models/users')
 const database = require('../db')
 
-exports.inserirUser = async (req, res) => {
-    await database.sync()
-    const body = req.body
 
-    if (body.name && body.password && body.email) {
-        Users.create({
-            name: body.name,
-            password: body.password,
-            email: body.email,
-            photo: body.photo,
-            weight: body.weight,
-            height: body.height
+module.exports = {
+    inserirUser: async (req, res) => {
+        await database.sync()
+        const body = req.body
+    
+        if (body.name && body.password && body.email) {
+            Users.create({
+                name: body.name,
+                password: body.password,
+                email: body.email,
+                photo: body.photo,
+                weight: body.weight,
+                height: body.height
+    
+            })
+            .then(data => {
+                res.send(data);
+            })
+            .catch(error => {
+                res.status(500).send({
+                    message: error.message || "Algo deu errado."
+                })
+            })
+        } else {
+            res.status(400).send({
+                message: 'Todos os campos precisam estar preenchidos.'
+            })
+        }
+    },
 
+    buscarUser: async (req, res) => {
+        await database.sync()  
+    
+        Users.findAll({ 
+            where: {
+                id: req.params.id
+            }
         })
         .then(data => {
-            res.send(data);
+            res.send(data)
         })
         .catch(error => {
             res.status(500).send({
-                message: error.message || "Algo deu errado."
+                message: error.message || 'Algo deu errado'
             })
         })
-    } else {
-        res.status(400).send({
-            message: 'Todos os campos precisam estar preenchidos.'
-        })
     }
-
-    
 }
 
-exports.findAll = async (req, res) => {
-    await database.sync()  
 
-    Users.findAll({ 
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(data => {
-        res.send(data)
-    })
-    .catch(error => {
-        res.status(500).send({
-            message: error.message || 'Algo deu errado'
-        })
-    })
-}
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const userController = require('../controllers/userController')
 
-router.get('/user/:id', userController.findAll)
+router.get('/user/:id', userController.buscarUser)
 router.post('/user', userController.inserirUser)
 
 module.exports = router
