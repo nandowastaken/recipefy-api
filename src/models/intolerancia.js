91d635916d8a05ad2406e06ac8f615baa24b3b9f
Eliel Cirino <elielscirino@gmail.com>
2023-03-13 16:08:08 -0300

    novos models (Dieta, Intolerancia)

---
+++ src/models/intolerancia.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize')
+const database = require('../db')
+
+const Intolerancia = database.define('Intolerancia', {
+    id: 
+    {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome:
+    {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    }
+})
+
+module.exports = Intolerancia
