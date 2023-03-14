3e9f01e672f5c779cf7977241b545f5e0a3e162c
Eliel Cirino <elielscirino@gmail.com>
2023-03-14 15:03:04 -0300

    server.use new routes

--- src/server.js
+++ src/server.js
@@ -3,11 +3,17 @@ const express = require('express')
 
 const userRoute = require('./routes/userRoute')
 const ingredienteRoute = require('./routes/ingredienteRoute')
+const dietaRoute = require('./routes/dietaRoute')
+const intoleranciaRoute = require('./routes/intoleranciaRoute')
+const unidadeRoute = require('./routes/unidadeRoute')
 
 const server = express()
 server.use(express.urlencoded({extended: false}))
 server.use(userRoute)
 server.use(ingredienteRoute)
+server.use(dietaRoute)
+server.use(intoleranciaRoute)
+server.use(unidadeRoute)
 
 const port = process.env.PORT
 server.listen(port, () => {
