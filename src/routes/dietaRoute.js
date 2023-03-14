76c33f29ab666cc88a21ba033ca97599c15b5c5c
Eliel Cirino <elielscirino@gmail.com>
2023-03-14 15:04:27 -0300

    dieta crud

---
+++ src/routes/dietaRoute.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const dietaController = require('../controllers/dietaController')
+
+router.get('/dietas', dietaController.buscarTodos)
+
+router.get('/dieta/:id', dietaController.buscarDieta)
+router.post('/dieta', dietaController.inserirDieta)
+router.put('/dieta/:id', dietaController.editarDieta)
+router.delete('/dieta/:id', dietaController.deletarDieta)
+
+
+module.exports = router
